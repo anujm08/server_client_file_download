612b3b2d95f1209e83bc5d9eb53d50bf48efeb36
Srajan Garg <srajan.garg@gmail.com>
2016-08-03 00:27:23 +0530

    add more functionality to client, only random/same remains

--- multi-client.c
+++ multi-client.c
@@ -4,8 +4,13 @@
 #include <netinet/in.h>
 #include <netdb.h> 
 #include <pthread.h>
+#include <time.h>
 
-static int portno;
+static int PORT;
+static int NUM_THREADS;
+static int RUN_TIME;
+static int SLEEP_TIME;
+static int NUM_FILES = 1000;
 static struct hostent *server;
 
 void error(char *msg)
@@ -19,65 +24,73 @@ void getFile()
     int sockfd, yes = 1;
     char buffer[256];
     struct sockaddr_in serv_addr;
+    time_t init = time(NULL);
 
-    /* create socket, get sockfd handle */
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
-    if (sockfd <= 0) 
-        error("ERROR opening socket\n");
-
-    bzero((char*)&serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-
-    bcopy((char*)server->h_addr, (char*)&serv_addr.sin_addr.s_addr, server->h_length);
-    serv_addr.sin_port = htons(portno);
-
-    /* connect to server */
-    if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) 
-        error("ERROR connecting\n");
-    printf("A client thread connected to the server\n", sockfd);
-
-    /* temporary get user input */
-    printf("Please enter the message: ");
-    bzero(buffer, 256);
-    fgets(buffer, 255, stdin);
-
-    /* send user message to server */
-    int bytes_sent = write(sockfd, buffer, strlen(buffer));
-    if (bytes_sent < 0) 
-        error("ERROR writing to socket\n");
-    bzero(buffer, 256);
-
-    while(1)
+    while (difftime(time(NULL), init) < RUN_TIME)
     {
-        int bytes_recv = recv(sockfd, buffer, sizeof(buffer), 0);
-        
-        if (bytes_recv < 0) 
-            error("ERROR reading from socket\n");
-        if (bytes_recv == 0)
+        /* create socket, get sockfd handle */
+        sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
+        if (sockfd <= 0) 
+            error("ERROR opening socket\n");
+
+        bzero((char*)&serv_addr, sizeof(serv_addr));
+        serv_addr.sin_family = AF_INET;
+
+        bcopy((char*)server->h_addr, (char*)&serv_addr.sin_addr.s_addr, server->h_length);
+        serv_addr.sin_port = htons(PORT);
+
+        /* connect to server */
+        if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) 
+            error("ERROR connecting\n");
+        printf("A client thread connected to the server\n", sockfd);
+
+        /* temporary get user input */
+        printf("Please enter the message: ");
+        bzero(buffer, 256);
+        fgets(buffer, 255, stdin);
+
+        /* send user message to server */
+        int bytes_sent = write(sockfd, buffer, strlen(buffer));
+        if (bytes_sent < 0) 
+            error("ERROR writing to socket\n");
+        bzero(buffer, 256);
+
+        while(1)
         {
-            printf("File received\n");
-            break;
+            int bytes_recv = recv(sockfd, buffer, sizeof(buffer), 0);
+            
+            if (bytes_recv < 0) 
+                error("ERROR reading from socket\n");
+            if (bytes_recv == 0)
+            {
+                printf("File received\n");
+                break;
+            }
         }
+
+        sleep(SLEEP_TIME);
     }
 }
 
 int main(int argc, char *argv[])
 {
-    if (argc < 3 || argc > 3) {
-       fprintf(stderr, "usage :  %s [host] [port]\n", argv[0]);
+    if (argc < 6 || argc > 6) {
+       fprintf(stderr, "usage :  %s [host] [port] [num-threads] [run time] [sleep time]\n", argv[0]);
        exit(0);
     }
 
-    // port
-    portno = atoi(argv[2]);
+    // TODO : sanity checks
+    PORT = atoi(argv[2]);
+    NUM_THREADS = atoi(argv[3]);
+    RUN_TIME = atoi(argv[4]);
+    SLEEP_TIME = atoi(argv[5]);
     
     // server    
     server = gethostbyname(argv[1]);
     if (server == NULL)
         error("ERROR no such host\n");
 
-    int NUM_THREADS = 3;
     pthread_t *tid = malloc(NUM_THREADS * sizeof(pthread_t));
 
     for(int i = 0; i < NUM_THREADS; i++) 
@@ -87,6 +100,8 @@ int main(int argc, char *argv[])
     for(int i = 0; i < NUM_THREADS; i++) 
         //change NULL for getting back return values
         pthread_join(tid[i], NULL);
+
+    // TODO : deallocate `tid`
 
     return 0;
 }
